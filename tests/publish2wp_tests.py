add6b5ab827f22a681231eb63e89c35d25ab0e2b
Balasankar C <balasankarc@autistici.org>
2015-06-21 01:01:37 +0530

    Fix tests

--- tests/publish2wp_tests.py
+++ tests/publish2wp_tests.py
@@ -6,7 +6,8 @@ def test_file():
     username = 'testuser'
     password = 'testuser'
     site = 'https://publish2wptesting.wordpress.com/'
-    testpublisher = publisher.publish2wp(username, password, site)
+    state = 'draft'
+    testpublisher = publisher.publish2wp(username, password, site, state)
     filestatus = testpublisher.getfile('tests/wrongfile.txt')
     assert_equal(filestatus, 'Invalid Syntax for input file')
     filestatus = testpublisher.getfile('tests/wrongfile2.txt')
@@ -21,17 +22,18 @@ def test_site():
     username = 'publish2wptest'
     password = 'publish2wptesting'
     site = 'https://publish2wptesting.wordpresss.com/'
-    testpublisher = publisher.publish2wp(username, password, site)
+    state = 'draft'
+    testpublisher = publisher.publish2wp(username, password, site, state)
     teststatus = testpublisher.connect()
     assert_equal(
         teststatus, "Can't connect. Check URL and protocol (http vs https)")
     site = 'http://publish2wptesting.wordpress.com/'
-    testpublisher = publisher.publish2wp(username, password, site)
+    testpublisher = publisher.publish2wp(username, password, site, state)
     teststatus = testpublisher.connect()
     assert_equal(
         teststatus, "Can't connect. Check URL and protocol (http vs https)")
     site = 'https://publish2wptesting.wordpress.com/'
-    testpublisher = publisher.publish2wp(username, password, site)
+    testpublisher = publisher.publish2wp(username, password, site, state)
     teststatus = testpublisher.connect()
     assert_equal(teststatus, 0)
 
@@ -40,18 +42,19 @@ def test_credential():
     username = 'publish2wptest'
     password = 'asdf'
     site = 'https://publish2wptesting.wordpress.com/'
-    testpublisher = publisher.publish2wp(username, password, site)
+    state = 'draft'
+    testpublisher = publisher.publish2wp(username, password, site, state)
     teststatus = testpublisher.connect()
     assert_equal(teststatus, 'Wrong Credentials')
     username = 'qewr'
     password = 'asdf'
     site = 'https://publish2wptesting.wordpress.com/'
-    testpublisher = publisher.publish2wp(username, password, site)
+    testpublisher = publisher.publish2wp(username, password, site, state)
     teststatus = testpublisher.connect()
     assert_equal(teststatus, 'Wrong Credentials')
     username = 'publish2wptest'
     password = 'publish2wptesting'
     site = 'https://publish2wptesting.wordpress.com/'
-    testpublisher = publisher.publish2wp(username, password, site)
+    testpublisher = publisher.publish2wp(username, password, site, state)
     teststatus = testpublisher.connect()
     assert_equal(teststatus, 0)
